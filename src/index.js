a60ede738c6025f4ceddca587760ee9643d832df
msyk9038 <41169937+msyk9038@users.noreply.github.com>
2022-02-27 17:24:32 +0900

    タスク登録の機能実装まで完了

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し，初期化する
+  const inputText = document.getElementById("addText").value;
+  document.getElementById("addText").value = "";
+  // alert(inputText); // check input value
+
+  //li 生成
+  const li = document.createElement("li");
+
+  //div 生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //p 生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //子要素の設定
+  li.appendChild(div);
+  div.appendChild(p);
+
+  //タスクリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("addButton")
+  .addEventListener("click", () => onClickAdd());
