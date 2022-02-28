b2f81993ee525e6419ccde75713bdb73a153caef
msyk9038 <41169937+msyk9038@users.noreply.github.com>
2022-02-28 12:37:03 +0900

    ToDo 内の done / delete ボタンの機能実装完了

--- src/index.js
+++ src/index.js
@@ -17,13 +17,64 @@ const onClickAdd = () => {
   const p = document.createElement("p");
   p.innerText = inputText;
 
+  //done button生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "done";
+  completeButton.addEventListener("click", () => {
+    const target = completeButton.parentNode.parentNode;
+    onDoneClick(target);
+  });
+
+  //delete button生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
+  deleteButton.addEventListener("click", () => {
+    const target = deleteButton.parentNode.parentNode;
+    onDeleteClick(target);
+  });
+
   //子要素の設定
   li.appendChild(div);
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //タスクリストに追加
   document.getElementById("incomplete-list").appendChild(li);
 };
+
+const onDoneClick = (target) => {
+  onDeleteClick(target);
+  const text = target.firstElementChild.firstElementChild.innerText;
+  //p 生成
+  const p = document.createElement("p");
+  p.innerText = text;
+  //back button 生成
+  const backButton = document.createElement("button");
+  backButton.innerText = "back";
+  backButton.addEventListener("click", () => {
+    alert("test");
+  });
+  target.firstElementChild.innerHTML = "";
+  target.firstElementChild.appendChild(p);
+  target.firstElementChild.appendChild(backButton);
+  document.getElementById("complete-list").appendChild(target);
+};
+
+const onDeleteClick = (target) => {
+  target.remove();
+};
+
+const onEnterInInputArea = (e) => {
+  if (e.keyCode === 13) {
+    onClickAdd();
+  }
+  return false;
+};
+
+document
+  .getElementById("addText")
+  .addEventListener("keypress", (e) => onEnterInInputArea(e));
 
 document
   .getElementById("addButton")
