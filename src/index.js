1ffd7571e0af94568118bab6e995907a200bc2ef
msyk9038 <41169937+msyk9038@users.noreply.github.com>
2022-03-01 08:27:10 +0900

    共通部分の関数化

--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ import "./styles.css";
 // タスク登録
 const onClickAdd = () => {
   // テキストボックスの値を取得し，初期化する
-  const inputText = document.getElementById("addText").value;
+  const text = document.getElementById("addText").value;
   document.getElementById("addText").value = "";
-  // alert(inputText); // check input value
-
   //li 生成
   const li = document.createElement("li");
-
   //div 生成
   const div = document.createElement("div");
   div.className = "list-row";
+  //li に div を子として設定
+  li.appendChild(div);
+  // div 内に タスクとボタンを生成
+  createIncompleteItem(div, text);
+  //タスクリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
 
+// div 内にタスクとボタンを生成
+const createIncompleteItem = (div, text) => {
   //p 生成
   const p = document.createElement("p");
-  p.innerText = inputText;
-
+  p.innerText = text;
   //done button生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "done";
@@ -25,29 +30,22 @@ const onClickAdd = () => {
     const target = completeButton.parentNode.parentNode;
     onDoneClick(target);
   });
-
   //delete button生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "delete";
   deleteButton.addEventListener("click", () => {
-    const target = deleteButton.parentNode.parentNode;
-    onDeleteClick(target);
+    deleteButton.parentNode.parentNode.remove();
   });
 
-  //子要素の設定
-  li.appendChild(div);
   div.appendChild(p);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-
-  //タスクリストに追加
-  document.getElementById("incomplete-list").appendChild(li);
 };
 
 // 完了ボタン
-const onDoneClick = (target) => {
-  onDeleteClick(target);
-  const text = target.firstElementChild.firstElementChild.innerText;
+const onDoneClick = (li) => {
+  li.remove();
+  const text = li.firstElementChild.firstElementChild.innerText;
   //p 生成
   const p = document.createElement("p");
   p.innerText = text;
@@ -58,44 +56,20 @@ const onDoneClick = (target) => {
     const target = backButton.parentNode.parentNode;
     onMoveBackClick(target);
   });
-  target.firstElementChild.innerHTML = "";
-  target.firstElementChild.appendChild(p);
-  target.firstElementChild.appendChild(backButton);
-  document.getElementById("complete-list").appendChild(target);
-};
-
-// 削除ボタン
-const onDeleteClick = (target) => {
-  target.remove();
+  li.firstElementChild.innerHTML = "";
+  li.firstElementChild.appendChild(p);
+  li.firstElementChild.appendChild(backButton);
+  document.getElementById("complete-list").appendChild(li);
 };
 
 // 戻すボタン
-const onMoveBackClick = (target) => {
-  onDeleteClick(target);
-  const text = target.firstElementChild.firstElementChild.innerText;
-  //p 生成
-  const p = document.createElement("p");
-  p.innerText = text;
-  //done / deleteボタン 生成
-  const completeButton = document.createElement("button");
-  completeButton.innerText = "done";
-  completeButton.addEventListener("click", () => {
-    const target = completeButton.parentNode.parentNode;
-    onDoneClick(target);
-  });
-
-  //delete button生成
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "delete";
-  deleteButton.addEventListener("click", () => {
-    const target = deleteButton.parentNode.parentNode;
-    onDeleteClick(target);
-  });
-  target.firstElementChild.innerHTML = "";
-  target.firstElementChild.appendChild(p);
-  target.firstElementChild.appendChild(completeButton);
-  target.firstElementChild.appendChild(deleteButton);
-  document.getElementById("incomplete-list").appendChild(target);
+const onMoveBackClick = (li) => {
+  li.remove();
+  const div = li.firstElementChild;
+  const text = div.firstElementChild.innerText;
+  div.innerHTML = "";
+  createIncompleteItem(div, text);
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 // エンター押したらタスク登録
